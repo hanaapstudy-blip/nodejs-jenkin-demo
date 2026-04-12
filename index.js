6e458b069a7d65484e8610af839dbf6c8a20e8da
hanaapstudy-blip <hanaapstudy@gmail.com>
2026-04-12 23:28:05 +0530

    2nd chnage

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http=require('http')
 http.createServer((req,res)=>{
     res.end("hello Jenkins CI/CD")
-}).listen(3000)
+}).listen(3000,()=>{
+    console.log('server is running on http://localhost:3000"')
+
+})
