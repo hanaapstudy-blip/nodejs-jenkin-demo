c675febf75e3ac332236ff0f3d73773c1c6768ae
hanaapstudy-blip <hanaapstudy@gmail.com>
2026-04-13 14:37:17 +0530

    8th change

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const http=require('http')
-const  server=http.createServer((req,res)=>{
-    res.end("hello Jenkins CI/CD")
-})
-// .listen(3000,()=>{
-//     console.log('server is running on http://localhost:3000"')
-
+// const http=require('http')
+// const  server=http.createServer((req,res)=>{
+//     res.end("hello Jenkins CI/CD")
 // })
-server.listen(3000,()=>{
-    console.log('server is running on http://localhost:3000')
-    setTimeout(()=>{
-        console.log('stopping server....')
-        process.exit(0)},2000)})
+// // .listen(3000,()=>{
+// //     console.log('server is running on http://localhost:3000"')
 
+// // })
+// server.listen(3000,()=>{
+//     console.log('server is running on http://localhost:3000')
+//     setTimeout(()=>{
+//         console.log('stopping server....')
+//         process.exit(0)},2000)})
+
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.end("hello Jenkins CI/CD");
+});
+
+server.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
