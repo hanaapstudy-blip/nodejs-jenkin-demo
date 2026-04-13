69d23dde959a1c3e1398d233bb8bbb0ed896d1e5
hanaapstudy-blip <hanaapstudy@gmail.com>
2026-04-13 09:41:04 +0530

    4th change

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const http=require('http')
 const  server=http.createServer((req,res)=>{
     res.end("hello Jenkins CI/CD")
 })
-// .listen(3000,()=>{
-//     console.log('server is running on http://localhost:3000"')
+.listen(3000,()=>{
+    console.log('server is running on http://localhost:3000"')
 
-// })
-server.listen(3000,()=>{
-    console.log('server is running on http://localhost:3000')
-    setTimeout(()=>{
-        console.log('stopping server....')
-        process.exit(0)},2000)
-    })
+})
+// server.listen(3000,()=>{
+//     console.log('server is running on http://localhost:3000')
+//     setTimeout(()=>{
+//         console.log('stopping server....')
+//         process.exit(0)},2000)})
 
